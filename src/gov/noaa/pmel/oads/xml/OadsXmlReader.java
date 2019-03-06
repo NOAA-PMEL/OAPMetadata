847240e53e2707ac1928936e297a9b3c9adf304a
linus.kamb <Linus.Kamb@noaa.gov>
2019-03-06 09:08:04 -0800

    checking in first draft of version a0.2

---
+++ src/gov/noaa/pmel/oads/xml/OadsXmlReader.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package gov.noaa.pmel.oads.xml;
+
+import java.io.FileReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+
+import gov.noaa.pmel.oads.OadsMetadataDocumentType;
+
+/**
+ * @author kamb
+ *
+ */
+public class OadsXmlReader {
+
+    /**
+     * 
+     */
+    public OadsXmlReader() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String filename = args.length > 0 ? args[0] : "versions/b0.1/sample_b0.1.xml";
+        try ( FileReader xmlReader = new FileReader(filename); ) {
+            JAXBContext content =  JAXBContext.newInstance(OadsMetadataDocumentType.class);
+            Unmarshaller unmarshaller = content.createUnmarshaller();
+            StreamSource source = new StreamSource(xmlReader);
+            Object obj =  unmarshaller.unmarshal(source, OadsMetadataDocumentType.class);
+            JAXBElement<OadsMetadataDocumentType> jobj =  (JAXBElement<OadsMetadataDocumentType>)obj;
+            Object maybe = jobj.getValue();
+            OadsMetadataDocumentType metadata = (OadsMetadataDocumentType) maybe;
+            System.out.println(metadata);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+}
